b3aa8b88482d31e4675fc9a5a81d0950714fa409
Asdy10 <asdyasdfghjkl@yandex.ru>
2023-09-22 21:05:16 +0300

    s

--- bot/loader.py
+++ bot/loader.py
@@ -1,8 +1,9 @@
+import os
+
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-from data import config
-
-bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+bot = Bot(token=BOT_TOKEN, parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
